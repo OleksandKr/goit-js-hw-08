c49915675349cfcdb25fed1c85f65aa6832fdf56
OleksandKr <cfif357@ukr.net>
2023-04-27 18:46:03 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGaleryEl = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+   </a>
+ </li>`;
+};
+
+galleryEl.innerHTML = galleryItems.map(makeGaleryEl).join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
